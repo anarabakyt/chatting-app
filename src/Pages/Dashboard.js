021953cebe78928aab8274e667cd3a44c822a1ad
Anara Akmatalieva <anara.bakyt@gmail.com>
2022-04-16 19:23:10 +0600

    final

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,4 +1,5 @@
-import { onValue, ref } from "firebase/database"
+import { child, onValue, push, ref, set } from "firebase/database"
+import moment from "moment"
 import { useEffect, useState } from "react"
 import { Navigate } from "react-router-dom"
 import { useAuthContext } from "../context/authContext"
@@ -7,6 +8,8 @@ import { db } from "../firebase/idex"
 export const Dashboard=()=>{
     const [users, setUsers]=useState()
     const {user,logout}=useAuthContext()
+    const [messageTxt,setMessageTxt]=useState('')
+    const [messages,setMessages]=useState([])
 
     const getUsers=()=>{
        
@@ -23,8 +26,71 @@ export const Dashboard=()=>{
       })
 
     }
+
+    const getMessages=()=>{
+       
+        onValue(ref(db,'messages'),(snapshot)=>{
+            const data=snapshot.val();
+            let arr=[]
+            Object.keys(data).map((key)=>{
+                arr.push({
+                    id:key,
+                    ...data[key],
+                });
+            })
+            setMessages(arr)
+        })
+  
+      }
+
+    const sendMessage=()=>{
+        if(messageTxt===''){
+            alert('Enter your message!')
+
+        }else{
+            let message={
+                text:messageTxt,
+                createdAt:moment().format('Do MMMM, HH:mm'),//15 april 15:00
+                sender:user?.displayName ?? user?.email
+            }
+            const key=push(child(ref(db), 'message')).key
+
+            set(ref(db, `messages/${key}`),message);
+            setMessageTxt('')
+        }
+
+    };
+
+    const sendLocation=()=>{
+        let options={
+            enableHighAccuracy:true,
+            timeout:5000,
+            maximumAge:0,
+        };
+        navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                console.log(position)
+                let latitude=position?.coords?.latitude;
+                let longitude=position?.coords?.longitude;
+
+                let mapUrl='https://www.google.com/maps?q='+latitude+","+longitude;
+
+                let message={
+                    url:mapUrl,
+                createdAt:moment().format('Do MMMM, HH:mm'),//15 april 15:00
+                sender:user?.displayName ?? user?.email
+                };
+                const key=push(child(ref(db), 'message')).key
+
+                set(ref(db, `messages/${key}`),message);
+            },error=>{
+                console.log(error)
+            },options
+        );
+    }
 useEffect(()=>{
-    getUsers()
+    getUsers();
+    getMessages()
 },[])
     return user ? (
         <div>
@@ -36,22 +102,26 @@ useEffect(()=>{
                       {users?.map((user)=>(
                           <li key={user?.id}>{user?.displayName?? user?.email}</li>
                       ))}
-                      <li>Anara</li>
+                     
                   </ol>
 
               </div>
               <div className="main-content">
                   <div className="messages">
-                      <div className="message">
-                          <p className="message-user">Admin <span className="message-time">5:20pm</span></p>
-                          <p className="message-text">Welcome to the chat app</p>
+                      {
+                          messages.map((message)=>(
+                      <div key={message?.id} className="message">
+                          <p className="message-user">{message?.sender} <span className="message-time">{message?.createdAt}</span></p>
+                          <p className="message-text">{message?.text ? (message?.text) : (<a target={"_blank"} href={message.url}>My location</a>)}</p>
 
                       </div>
+                      ))
+                    }
                   </div>
                   <div className="messages-footer">
-                      <input className="message-input" placeholder="Message"/>
-                      <button className="msg-button">Send</button>
-                      <button className="msg-button">Send Location</button>
+                      <input className="message-input" placeholder="Message" onChange={(e)=>setMessageTxt(e.target.value)} value={messageTxt}/>
+                      <button className="msg-button" onClick={sendMessage}>Send</button>
+                      <button className="msg-button" onClick={sendLocation}>Send Location</button>
                   </div>
               </div>
 
